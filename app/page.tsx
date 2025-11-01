d8574e0f91d0617593f62e93a6b6cd734645823e
rifki <rifkihidayat23@yahoo.com>
2025-11-02 02:58:34 +0700

    change image path

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export const metadata = {
     siteName: "Sumbawa Rental Mobil",
     images: [
       {
-        url: "/hilux2.png",
+        url: "/hilux.png",
         width: 1200,
         height: 630,
         alt: "Toyota Hilux putih - Sumbawa Rental Mobil",
